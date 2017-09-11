ee61cea1a58c90f712ac0f032cb4a68f8615dc88
Jim Evans <james.h.evans.jr@gmail.com>
2017-09-11 13:04:50 -0700

    Initial commit

---
+++ CoreDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using StandardDemo;
+using System;
+
+namespace CoreDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Parser p = new Parser("{'prop': 'value'}");
+            string v = p.GetValue("prop");
+            Console.WriteLine(v);
+        }
+    }
+}
---
+++ StandardDemo/Parser.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace StandardDemo
+{
+    public class Parser
+    {
+        Dictionary<string, string> dictionary;
+        public Parser(string jsonString)
+        {
+            this.dictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonString);
+        }
+
+        public string GetValue(string prop)
+        {
+            return this.dictionary[prop];
+        }
+    }
+}
